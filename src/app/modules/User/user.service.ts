f2f2a02b7d60c8e51080b9d54e1d441ec92caeda
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2024-03-14 14:58:39 +0600

    response updated

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,7 +1,7 @@
 import { User } from './user.model';
 
 const getMe = async (userId: string) => {
-          const user = await User.findById(userId).select('-_id -password -isDeleted -status -createdAt -updatedAt').lean().exec();
+          const user = await User.findById(userId).select('-password -isDeleted -status -createdAt -updatedAt').lean().exec();
 
           return user;
 };
